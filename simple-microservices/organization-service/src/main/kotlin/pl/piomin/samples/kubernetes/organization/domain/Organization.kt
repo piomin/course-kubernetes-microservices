2f0ca7b46f1bf510bc30cf27ab516297ee030677
piomin <piotr.minkowski@gmail.com>
2020-08-06 16:44:47 +0200

    fix organization

--- simple-microservices/organization-service/src/main/kotlin/pl/piomin/samples/kubernetes/organization/domain/Organization.kt
+++ simple-microservices/organization-service/src/main/kotlin/pl/piomin/samples/kubernetes/organization/domain/Organization.kt
@@ -6,6 +6,6 @@ import javax.persistence.Id
 
 @Entity
 data class Organization(@Id @GeneratedValue var id: Int? = null,
-                        val name: String,
+                        val name: String = "",
                         @Transient val employees: MutableSet<Employee> = mutableSetOf(),
                         @Transient val departments: MutableSet<Department> = mutableSetOf())
